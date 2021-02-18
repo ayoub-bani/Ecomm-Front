f4d8f392051a75c45b798462708f7a412d0a9f36
ayoub bani <ayoub.b@dsi-cs.com>
2021-02-18 14:11:07 +0100

    added footer & header components

---
+++ components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="custom-footer-color h-44 text-white">this is the Footer</div>
+</template>
+
+<script>
+  export default {
+
+  }
+
+</script>
+
+<style>
+  /* Footer color */
+  .custom-footer-color {
+    background-color: #373839;
+  }
+
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+.<template>
+  <div class="bg-gray-400">this is the Header</div>
+</template>
+
+<script>
+  export default {
+
+  }
+
+</script>
+
+<style>
+
+</style>
